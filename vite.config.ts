4fb5f9614dd0310f25d95cb0fc79b1bc4cd91014
Leighton Payne <leightonpayne98@gmail.com>
2026-01-15 16:26:57 +0100

    build: make 'npm run dev' open browser

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/plasmid_network/",
   plugins: [react(), tailwindcss()],
+  server: {
+    open: "/plasmid_network/",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
